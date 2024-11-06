ecee15090ee0b0cb54324c2770d40a4ef759b216
Guilherme Justo <guilherme-justo@hotmail.com>
2024-11-06 16:32:39 -0300

    Project creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './app/components/components.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    MenuBarComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
