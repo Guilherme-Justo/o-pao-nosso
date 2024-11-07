ee3c5b3141c3f26668e39728f88edbbf28ecf01b
Guilherme Justo <guilherme-justo@hotmail.com>
2024-11-07 11:10:58 -0300

    Editing the first components for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './app/components/components.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     MenuBarComponent,
-    MenuTitleComponent
+    MenuTitleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
